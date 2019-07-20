366f21191a053bda219b61859500e7667c80e2ad
cclivick <46388110+cclivick@users.noreply.github.com>
2019-07-20 13:30:58 -0500

    Updated test file due to .env issue

---
+++ test.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+console.log(require("dotenv").config());
+var axios = require("axios");
+var keys = require("./APIkeys.js");
+
+console.log(keys);
+
+function firstCall(string) {
+  axios.get("http://api.wolframalpha.com/v2/query?input=" + string + "&format=image,plaintext&output=JSON&appid=" + keys) 
+    .then(function(response) {
+      console.log(response.data.queryresult.success);
+    });
+};
+
+firstCall("germany+vs+france");
