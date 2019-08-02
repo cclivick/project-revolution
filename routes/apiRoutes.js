20c0f70d1dd67e48b60ee5b460b66faa9e418d1a
cclivick <46388110+cclivick@users.noreply.github.com>
2019-08-02 18:13:43 -0500

    Updated README. Still need to add gifs, etc.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -131,12 +131,5 @@ module.exports = function(app) {
           res.json(data);
     });
   });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
 };
 
